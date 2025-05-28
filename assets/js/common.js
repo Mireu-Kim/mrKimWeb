2d5c943b6e6421ae2e98e0e8e29934581bb6d105
김미르 <mrkim@wylie.co.kr>
2025-05-28 17:49:24 +0900

    작업중

---
+++ assets/js/common.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const skillList = document.querySelector('.skill');
+  if (skillList) {
+    skillList.innerHTML += skillList.innerHTML;
+  }
+});
