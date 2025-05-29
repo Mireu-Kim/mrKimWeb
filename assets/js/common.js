b9a5a3523878ad8532c88cee2744fbd010977320
김미르 <mrkim@wylie.co.kr>
2025-05-29 17:08:45 +0900

    작업중

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,3 +4,20 @@
 //     skillList.innerHTML += skillList.innerHTML;
 //   }
 // });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const topButton = document.querySelector(".btn_top");
+
+	window.addEventListener("scroll", function () {
+		if (window.scrollY === 0) {
+			topButton.style.display = "none";
+		} else {
+			topButton.style.display = "block";
+		}
+	});
+});
+
+function scrollIntoStart(id) {
+	const x = document.getElementById(id);
+	x.scrollIntoView({ behavior: "smooth", block: "start" });
+}
